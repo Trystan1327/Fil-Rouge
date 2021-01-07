2959a382ebcaf56f93f2ae127f8b5692e432acf3
Trystan <t.desousa0377@gmail.com>
2021-01-07 10:36:25 +0100

    Filrouge

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+include('header.php');
+?>
+
+     <div>
+        <img src="public/images/guitare.png" alt="photo" id="pubguitare">
+        <img src="public/images/bnd.png" alt="photo" id="prixdroit">
+        
+    
+    </div>
+   
+        <div class="nav4">
+            <img src="public/images/bnc.png" alt="photo" id="bcentre " >
+            <a href="villagegreen\categories.php" title="liens" id="cat">Nos Catégories</a>
+        </div> 
+            <div class="nav5">
+                <img src="public/images/guitare1.png" class="img-fluid">
+                <img src="public/images/batterie.png" class="img-fluid">
+                <img src="public/images/piano.png" class="img-fluid">
+                <img src="public/images/micro.png" class="img-fluid">
+                </div>
+                    <div class="nav6">    
+                        <img src="public/images/sono.png" class="img-fluid" >
+                        <img src="public/images/cases.png" class="img-fluid" width="24%">
+                        <img src="public/images/cable.png" class="img-fluid" >
+                        <img src="public/images/saxo.png" class="img-fluid" >
+                    </div> 
+        <div class="cat">
+            <div class="row" >
+                    <a href="green\application\views\meilleuresVentes.php" title="liens" id="cat">Nos Meilleures Ventes</a>
+                    <a href="green\application\views\sponsor.php" title="liens" id="vente">Nos  Partenaires</a>
+            </div>
+        </div>
+<div class="space">
+    <div class="row ">
+        <img src="public/images/venteguitare.png"id="one">
+        <img src="public/images/ventesaxo.png" id="two">
+        <img src="public/images/ventepiano.png" id="three">
+     
+        <img src="public/images/4logos.png" id="four">
+    </div>     
+</div>
+        
+<?php
+include('footer.php');
+?>
